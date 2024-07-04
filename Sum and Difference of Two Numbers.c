b4746482914b121f59b2ae318ccfa700c5eb2c54
Gustavomedeirost <136112308+Gustavomedeirost@users.noreply.github.com>
2024-07-04 17:18:54 -0300

    Sum and Difference of Two Numbers.c

--- Sum and Difference of Two Numbers.c
+++ Sum and Difference of Two Numbers.c
@@ -5,20 +5,16 @@
 
 int main()
 {
-    int a = 10;
-    int b = 4;
-    float c = 4.0;
-    float d = 2.0;
-    
-   
-    printf("%d %d\n", a + b, a - b);
+	int a,b;
+    float c,d;
+
     scanf("%d", &a);
-    scanf("&d", &b);
+    scanf("%d", &b);
+    printf("%d %d\n", a + b, a - b);
     
-    
-    printf("%.1f %.1f", c + d, c - d);
     scanf("%f", &c);
     scanf("%f", &d);
-    
+    printf("%.1f %.1f", c + d, c - d);
+        
     return 0;
 }
